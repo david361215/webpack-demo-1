b6c1078697c87a7b8757007d1032f94c31864407
david361215 <1138890119@qq.com>
2022-01-11 18:01:46 +0800

    split webpack.config.js into three files

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,27 +1,19 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
+const base = require('./webpack.config.base.js')
+
 module.exports = {
-  mode: 'production',
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.[contenthash].js',
-  },
-  devServer: {
-    contentBase: './dist',
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-        filename:'[name].[contenthash].css',
-        chunkFilename:'[id].[contenthash].css',
-        ignoreOrder:false,
-    }),  
-    new HtmlWebpackPlugin({
-    title: 'XDML - 写代码啦',
-    template: 'src/assets/index.html'
-  })],
+    ...base,
+    mode: 'production',
+    plugins: [
+        ...base.plugins,
+        new MiniCssExtractPlugin({
+            filename:'[name].[contenthash].css',
+            chunkFilename:'[id].[contenthash].css',
+            ignoreOrder:false,
+        })
+    ],
     module: {
         rules: [
             {
